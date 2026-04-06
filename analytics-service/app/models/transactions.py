881be5c980f590e8362a32604ebe083b6041492b
Maksim Seryakov <seryakov_2014@mail.ru>
2026-04-06 13:48:40 +0300

    fix: update migration

--- analytics-service/app/models/transactions.py
+++ analytics-service/app/models/transactions.py
@@ -11,7 +11,7 @@ from app.core.database import Base
 class TransactionsOrm(Base):
     __tablename__ = 'transactions'
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     payment_id: Mapped[uuid.UUID] = mapped_column(unique=True)
     amount: Mapped[Decimal]
     currency: Mapped[str]
